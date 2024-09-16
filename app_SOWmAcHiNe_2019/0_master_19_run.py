bef732f901c8348409ed30f5c6956a847bf1f3a5
sudz4 <mwsuther@syr.edu>
2024-09-15 23:29:57 -0700

    dark dragon 7

--- app_SOWmAcHiNe_2019/0_master_19_run.py
+++ app_SOWmAcHiNe_2019/0_master_19_run.py
@@ -3,13 +3,13 @@ import glob
 import papermill as pm
 import re
 
-# Specify the directory containing your notebooks
+# jupyter notebook directory
 notebooks_dir = '/Users/sudz4/Desktop/BOOK-II/servicenow-interview-proj/app_SOWmAcHiNe_2019'  # Replace with your directory path
 
-# Change the current working directory to the notebooks directory
+# change pwd to the notebooks directory
 os.chdir(notebooks_dir)
 
-# Retrieve all .ipynb files
+# retrieve all .ipynb files
 notebook_files = glob.glob('*.ipynb')
 
 # sort the notebooks by name
@@ -19,18 +19,18 @@ for nb in notebook_files:
     print(nb)
 
 
-# Execute notebooks sequentially
+# execute notebooks sequentially
 for idx, nb in enumerate(notebook_files, 1):
     print(f"\nExecuting notebook {idx}/{len(notebook_files)}: {nb}")
     try:
         pm.execute_notebook(
             input_path=nb,
-            output_path=nb,  # Overwrite the notebook with output; change if you want to keep the original unmodified
-            parameters={},    # Pass parameters here if needed
+            output_path=nb,  # overwrite option?
+            parameters={},    # pas params if needed?
             progress_bar=True
         )
-        print(f"Successfully executed {nb}")
+        print(f"Statement of Work (SOW) generated successfully {nb}")
     except Exception as e:
         print(f"Error executing {nb}: {e}")
-        # Decide whether to continue or halt execution
-        break  # Use 'continue' if you want to proceed with the next notebook despite the error
+        # ???? decide whether to continue or halt execution
+        break  # use 'continue' to proceed with the next notebook despite the error ?
